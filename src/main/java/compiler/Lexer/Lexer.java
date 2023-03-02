67a7404af0553839a8bc524988db442c721854a8
Robin Paquet <robin.paquet@student.uclouvain.be>
2023-03-02 13:02:15 +0100

    add some case for symbol

--- src/main/java/compiler/Lexer/Lexer.java
+++ src/main/java/compiler/Lexer/Lexer.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Lexer {
 
     private int count;
-    final static byte EOI = 0x1A;
+    final static byte EOF = 0x1A;
     char[] array = new char[100];
 
     public Lexer(Reader input){
@@ -16,27 +16,47 @@ public class Lexer {
         }
     }
     public int get_char(int i){
-        return i < array.length ? this.array[i] : EOI;
+        return i < array.length ? this.array[i] : EOF;
     }
 
     public Symbol getNextSymbol() {
         while(true){
-            System.out.println((char) get_char(count));
             switch (get_char(count)){
+                case EOF:
+                    System.out.println("EOF");
+                    return null;
                 case ' ':
+                    // Case of space
                     break;
                 case '\t':
+                    // Case of tab
+                    break;
                 case '\n':
+                    // Case of new line
+                    break;
+                case ';':
+                    System.out.println("got an end line (;)");
+                    break;
                 case '/':
                     if(get_char(++count)=='/'){
                         // Case of comment
                         System.out.println("got a comment (//)");
                     }
-                case ';':
-                    System.out.println("got an end line (;)");
-                case EOI:
-                    System.out.println("EOF");
-                    return null;
+                case '=':
+                    System.out.println("Assignement '='");
+                default:
+                    // Case of identifier
+                    // Case of keyword
+                    // Case of value
+                    int c = get_char(count);
+                    if(c >= 'A' && c <= 'Z'
+                        || c >= 'a' && c <= 'z'
+                        || c == '_'){
+                        System.out.println("letter : " + (char) c);
+                    }
+                    if(c >= '0' && c <= '9') {
+                        System.out.println("number : " + (char) c);
+                    }
             }
             count++;
         }
