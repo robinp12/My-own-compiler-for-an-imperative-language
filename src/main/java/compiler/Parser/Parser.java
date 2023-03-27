b9045f5a976bfc7cf90465930019fa24172ccdc2
guillaumespronck <81221734+guillaumespronck@users.noreply.github.com>
2023-03-27 19:18:46 +0200

    Add files via upload

--- src/main/java/compiler/Parser/Parser.java
+++ src/main/java/compiler/Parser/Parser.java
@@ -9,19 +9,31 @@ import compiler.Parser.AST.Program;
 import java.text.ParseException;
 
 public class Parser {
-    private static Program root;
     private static Lexer lexer;
     public static Symbol lookahead;
 
     public Parser(Lexer lexer) throws ParseException {
-        Parser.lexer = lexer;
-        lookahead = lexer.getNextSymbol();
-        root = new Program();
+        this.lexer = lexer;
+        this.lookahead = lexer.getNextSymbol();
     }
 
     /* Must return root of AST */
-    public Symbol getAST(){
-        return null;
+    public ExpressionNode getAST(){
+        List<ExpressionNode> expressions = new ArrayList<>();
+
+        while (!isAtEnd()) {
+            try {
+                ExpressionNode expr = parseExpr();
+                expressions.add(expr);
+            } catch (SyntaxErrorException e) {
+                // Report the error and continue parsing
+                System.err.println(e.getMessage());
+                synchronize();
+            }
+            //this.lookahead = lexer.getNextSymbol();
+        }
+
+        return new ProgramNode(expressions);
     }
 
     public static Symbol match(SymbolKind token) throws ParseException {
@@ -34,5 +46,13 @@ public class Parser {
             lookahead = lexer.getNextSymbol();
             return matchingSymbol;
         }
+    }
+
+    private boolean isAtEnd() {
+        return lookahead.equals(null);
+    }
+
+    private parseExpr(){
+        
     }
 }
