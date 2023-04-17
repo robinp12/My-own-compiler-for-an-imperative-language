9c3af4bb4a62aebf09e20ba4db9653dd08fd8573
Robin Paquet <44506208+robinp12@users.noreply.github.com>
2023-04-17 19:02:16 +0200

    Update MethodNode.java

--- src/main/java/compiler/Parser/AST/MethodNode.java
+++ src/main/java/compiler/Parser/AST/MethodNode.java
@@ -11,12 +11,12 @@ import static compiler.Parser.Parser.match;
 public class MethodNode extends ExpressionNode {
     //TODO
 
-    private Symbol identifier;
+    private String identifier;
     private TypeNode returnType;
     private ArrayList<ParamNode> parameters;
     private BlockNode body;
 
-    public MethodNode(Symbol identifier, TypeNode returnType, ArrayList<ParamNode> parameters, BlockNode body) {
+    public MethodNode(String identifier, TypeNode returnType, ArrayList<ParamNode> parameters, BlockNode body) {
         super(returnType.getTypeSymbol());
         this.identifier = identifier;
         this.returnType = returnType;
@@ -29,7 +29,7 @@ public class MethodNode extends ExpressionNode {
     public BlockNode getBody() {
         return body;
     }
-    public Symbol getIdentifier() {
+    public String getIdentifier() {
         return identifier;
     }
     public TypeNode getReturnType() {
@@ -38,13 +38,13 @@ public class MethodNode extends ExpressionNode {
 
     public static MethodNode parseMethod() throws ParseException{
         match(SymbolKind.PROC);
-        Symbol name = match(SymbolKind.LITERAL);
+        String identifier = LiteralNode.parseLiteral().getLiteral();
         match(SymbolKind.LPAR);
         ArrayList<ParamNode> params = ParamListNode.parseParams();
         match(SymbolKind.RPAR);
         TypeNode returnType = TypeNode.parseType();
         BlockNode body = BlockNode.parseBlock();
-        return new MethodNode(name, returnType, params, body);
+        return new MethodNode(identifier, returnType, params, body);
     }
 
     @Override
