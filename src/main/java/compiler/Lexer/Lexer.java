1fc0da92b37eba25cb5b0c7ea7b1230cdd65ae68
guillaumespronck <81221734+guillaumespronck@users.noreply.github.com>
2023-05-05 16:16:40 +0200

    Improve semantic analysis

--- src/main/java/compiler/Lexer/Lexer.java
+++ src/main/java/compiler/Lexer/Lexer.java
@@ -63,7 +63,7 @@ public class Lexer {
             switch (c){
                 case '0': case '1': case '2': case '3':
                 case '4': case '5': case '6': case '7':
-                case '9': case '.': { num += String.valueOf(c); i++; break;}
+                case '8': case '9': case '.': { num += String.valueOf(c); i++; break;}
                 default:
                     return num;
             }
