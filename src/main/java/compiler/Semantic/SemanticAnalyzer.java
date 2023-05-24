081a4cca591817db4804196bf0a0e3d21a22530d
gspronck <guillaume.spronck@student.uclouvain.be>
2023-05-24 10:02:27 +0200

    Small fix

--- src/main/java/compiler/Semantic/SemanticAnalyzer.java
+++ src/main/java/compiler/Semantic/SemanticAnalyzer.java
@@ -121,12 +121,6 @@ public class SemanticAnalyzer implements ASTVisitor {
         String rightType = node.getRight().getTypeStr();
 
         System.out.println("node "+ node.getLeft());
-        if(node.getLeft() instanceof LiteralNode && node.getRight() instanceof LiteralNode){
-            return "";
-        }
-        if(node.getLeft() instanceof LiteralNode || node.getRight() instanceof LiteralNode){
-            return "";
-        }
 
         if (leftType.equals("binaryExp")) {
             leftType = visit((BinaryExpressionNode) node.getLeft());
@@ -444,6 +438,7 @@ public class SemanticAnalyzer implements ASTVisitor {
     @Override
     public void visit(ExpressionNode node) throws Exception {
         System.out.println("expression");
+        System.out.println(node);
         if (node instanceof VarDeclarationNode) {
             visit((VarDeclarationNode) node);
         } else if (node instanceof ValDeclarationNode) {
